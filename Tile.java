2fb1b259554dcf650e0fb4f9f88ccfa690083002
lolitanaz314 <ln3141592653@gmail.com>
2019-11-24 17:58:56 -0500

    Update Tile.java

--- Tile.java
+++ Tile.java
@@ -1,18 +1,44 @@
 package GameComponents;
+import java.util.*;
 import java.util.Iterator;
 
 import Squares.Square;
 
 public class Tile {
 	
+	/*
+	public static void main(String [] args) {
+		
+		
+		ArrayList<Tile> list = new ArrayList<>();
+		
+		list.add(new Tile('a', 2));
+		list.add(new Tile('b', 3));
+		list.add(new Tile('d', 4));
+	
+		Tile t = new Tile('b', 3);
+		
+		System.out.println(list.size());
+		for (int i = 0; i <= list.size() + 1; i++) {
+			
+			for (Tile x : list)
+				System.out.print(x + " ");
+			
+			System.out.println();
+			
+			list.remove(list.get(0));
+		}
+		
+		System.out.println(list.size());
+	}
+	*/
+	
 	private char letter;
 	private int letterPoints;
-	private Square assignedSquare;
 	
 	public Tile() {
 		letter = '_';
 		letterPoints = 0;
-		assignedSquare = null;
 	}
 	
 	public Tile(char letter, int letterPoints) {
@@ -24,16 +50,19 @@ public class Tile {
 		return letter;
 	}
 	
-	public int getLetterPoints() {
-		return letterPoints;
+	public void setLetter(char ch) {
+		this.letter = ch;
 	}
 	
-	public Square getAssignedSquare() {
-		return assignedSquare;
+	public int getLetterPoints() {
+		return letterPoints;
 	}
 	
 	public String toString() {
 		return "" + letter;
 	}
-
+	
+	public boolean equals(Tile t) {
+		return (this.getLetter() == t.getLetter());
+	}
 }
