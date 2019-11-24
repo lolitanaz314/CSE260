c5805474e0e428d2c6f06ef618fad38ebc04c26d
lolitanaz314 <ln3141592653@gmail.com>
2019-11-24 17:58:07 -0500

    Update Dictionary.java

--- Dictionary.java
+++ Dictionary.java
@@ -1,28 +1,71 @@
 package GameComponents;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Dictionary {
 	
-    static Set<String> dictionarySet = new HashSet<String>(Arrays.asList("a", "able", "above", "afraid", "after", "again", "always",
-            "among", "appear", "answer", "baby", "back", "band", "ball", "basic", "beauty", "before", "began",
-            "between", "bird", "black", "blood", "blue", "board", "bottom", "busy", "came",
-            "captain", "car", "carry", "catch", "caught", "cause", "cell", "certain", "character", "claim",
-            "clean", "clear", "climb", "company", "common", "complete", "control", "corn", "correct", "copy",
-            "cotton", "count", "create", "current", "dance", "danger", "decide", "depend", "determine", "dictionary",
-            "difficult", "discuss", "distant", "division", "dollar", "door", "draw", "dream", "dress", "drink", "during",
-            "edge", "egg", "element", "enemy", "energy", "engine", "experience", "exercise", "experiment", "famous",
-            "favor", "fear", "field", "field"));
-    
-    public Dictionary() {
-    	this.dictionarySet = dictionarySet;
-    }
-    
-    public boolean isValidWord(String word) {
-    	return dictionarySet.contains(word);
-    }
-    
-    
+	ArrayList<String> listOfWords = new ArrayList<>();
+	
+	
+	// just to test
+	/*
+	public static void main(String [] args) throws FileNotFoundException {
+		
+		Dictionary newDictionary = new Dictionary();
+		
+		System.out.println(newDictionary.isValidWord("pie"));
+		
+	}
+	*/
+	
 
+	// adds ALL of the dictionary words in text file to the listOfWords ArrayList
+	
+	public Dictionary() throws FileNotFoundException
+	{
+		FileReader reader2 = new FileReader("wordList.txt");
+		Scanner in2 = new Scanner(reader2);
+		
+		for(int count1=0; in2.hasNextLine(); count1++)
+		{
+		    listOfWords.add(in2.nextLine());
+		}
+		
+		in2.close();
+	}
+	
+	// basically a binary search
+	// don't need to sort bc the list is already sorted
+	
+	public boolean isValidWord (String word) {
+		
+		int index = Collections.binarySearch(listOfWords, word);
+		
+		if (index < 0) {
+			return false;
+		}
+		return true;
+	}
+	   
+   // just to test
+	
+   /*
+   public ArrayList<String> getlistOfWords()
+   {
+	   ArrayList<String> testList = new ArrayList<>();
+		for (int i = 0; i < 5; i++) {
+			testList.add(listOfWords.get(i));
+		}
+		
+		return testList;
+   }
+   */
+	   
 }
