c7c03137eafde9b002c18a2028d1442c5de3600b
lolitanaz314 <ln3141592653@gmail.com>
2019-12-01 11:04:49 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,56 +1,91 @@
+// MINISCRABBLE
+
 package GameComponents;
 import java.util.ArrayList;
 
 public class Player {
 	
-	Tile [] rack;
+	ArrayList <Tile> rack;
 	private int score;
 	private String name;
-	private Bag bag = new Bag();
+	private boolean turn;
 	
 	public Player(String newName) {
 		
-		name = newName;
-		
-		rack = new Tile[7];
-		
-		// player gets 7 tiles from the bag
-		
-		for (int i = 0; i < 7; i++)
-			rack[i] = bag.pullTile();
+		this.name = newName;
+		this.turn = false;
+		this.rack = new ArrayList<Tile>();
 	}
 	
-	public Tile[] getRack () {
+	public boolean getTurn() {
+		return turn;
+	}
+	
+	public void changeTurn() {
+		if (turn)
+			turn = false;
+		else
+			turn = true;
+	}
+	
+	public ArrayList<Tile> getRack () {
 		
-		for (int i = 0; i < rack.length; i++)
-			System.out.print(rack[i] + " ");
+		for (int i = 0; i < rack.size(); i++)
+			System.out.print(rack.get(i) + " ");
 		
 		return rack;
 	}
-	/*
-    // if player doesn't like their tiles in the rack
-	public Tile [] exchangeTilesInRack() {
-		
+	
+	public void paintRack() {
+		for (int i = 0; i < rack.size(); i++)
+			System.out.print(rack.get(i) + " ");
 	}
-	*/
 	
 	// checks whether user letter input can be put on the board
 	public boolean letterInRack(char letter) {
 		
-		for (int i = 0; i < rack.length; i++)
-			if (letter == rack[i].getLetter())
+		for (int i = 0; i < rack.size(); i++)
+			if (letter == rack.get(i).getLetter())
 				return true;
 		
 		return false;
+	}
+	
+	public void removeTile(Tile tile) {
+		
+		for (int i = 0; i < rack.size(); i++) {
+			if (rack.get(i).equals(tile)) {
+				rack.remove(rack.get(i));
+				break;
+			}
+		}
+	}
+	
+	
+	// for exchanging tiles in the rack
+	public Tile removeAndReturnTile(Tile tile) {
+		
+		Tile t = new Tile();
+		for (int i = 0; i < rack.size(); i++) {
+			if (rack.get(i).equals(tile)) {
+				t = rack.get(i);
+				rack.remove(rack.get(i));
+				break;
+			}
+		}
+		
+		return t;
 	}
 	
 	public int getScore() {
 		return score;
 	}
 	
+	public void changeScore(int value) {
+		score += value;
+	}
+	
 	public String getName() {
 		return name;
 	}
-	
-	
 }
